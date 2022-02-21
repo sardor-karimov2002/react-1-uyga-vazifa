45a323bd21df93a84ef4907734f2e7e537b5a471
MJurayev <jurayevmansurbek667@gmail.com>
2022-02-21 16:42:30 +0500

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import "./App.css"
+import Footer from "./components/Footer/Footer"
+import Header from "./components/Header/Header"
+// class -> className
+// for -> htmlFor
+// snake case -> camelCase
+
+function App() {
+  
+  return (
+    <div className="App">
+      <Header />
+      Main Content
+      <Footer />
+  </div>
+  )
+ }
+
+export default App
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,9 @@
+import "./Header.css"
+
+function Header() {
+    return <div className="header">
+        Navbar component
+    </div>
+}
+
+export default Header
